50bb99d1bdca8535d0ea98069d6a3cc5fcb5cddf
bijaydeo <deo.developer@gmail.com>
2016-01-03 22:59:09 -0500

    refactor

--- src/main/java/com/bijay/compose_method/after/DiscountCalculator.java
+++ src/main/java/com/bijay/compose_method/after/DiscountCalculator.java
@@ -3,10 +3,12 @@ import com.bijay.method_object.after.Order;
 
 public class DiscountCalculator {
         public static float invoke(Order order) {
+
             java.util.Properties discountProperties = getDiscountProperties();
             int defaultQuantity = 5;
             double minimumPrice = 100.00;
             float discount = 0.2f;
+
             int discountQuantity = Integer.parseInt(discountProperties.getProperty("quantity")) == 0 ?defaultQuantity:Integer.parseInt(discountProperties.getProperty("quantity"));
             double minimumDiscountPrice = Integer.parseInt(discountProperties.getProperty("minimum.price")) == 0 ?minimumPrice:Integer.parseInt(discountProperties.getProperty("minimum.price"));
 
